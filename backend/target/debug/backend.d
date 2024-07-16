1319089dac8de87c7f5d9e0bb4b34542156b61fe
ngaga <nicolasgms6@gmail.com>
2024-07-16 17:18:50 +0200

    backend: initialize backend with "cargo new backend".

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+/home/ngaga/work/thirdparty/slime-volley/backend/target/debug/backend: /home/ngaga/work/thirdparty/slime-volley/backend/src/main.rs
---
+++ backend/target/debug/deps/backend-9bce9613bc66f20f.d
@@ -0,0 +1,5 @@
+/home/ngaga/work/thirdparty/slime-volley/backend/target/debug/deps/backend-9bce9613bc66f20f: src/main.rs
+
+/home/ngaga/work/thirdparty/slime-volley/backend/target/debug/deps/backend-9bce9613bc66f20f.d: src/main.rs
+
+src/main.rs:
